abb4c8e335500d48f98e6d109f8258ba8cb49283
Chad Harper <chadharper811@gmail.com>
2024-10-05 08:18:20 -0500

    added some starting variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
